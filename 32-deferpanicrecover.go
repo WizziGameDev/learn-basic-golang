839a5156db9ae1473cc457b464a949d0fd76ca6d
WizziGameDev <rickyadamsaputra1.com>
2025-08-02 15:46:28 +0700

    update panic

--- 32-deferpanicrecover.go
+++ 32-deferpanicrecover.go
@@ -1,29 +1,21 @@
 package main
 
-// defer will be run after function done
-func deferAlert() string {
-	return "CODE DONE"
-}
+import "fmt"
 
-func testDefer() string {
-	defer deferAlert()
-	print("Test defer alert")
-	return "Defer after this print"
+// defer will be run after function done
+func deferAlert() {
+	fmt.Println("Code End")
 }
 
 // panic will be run and code really break/stop
-func panicAlert() string {
-	return "ERROR CODE"
-}
-
-func testPanic(test bool) string {
+func runApp(test bool) string {
+	defer deferAlert()
 	if test {
-		return panicAlert()
+		panic("Panic code break!")
 	}
 	return "Test panic"
 }
 
 func main() {
-	testDefer()
-	testPanic(true)
+	runApp(true)
 }
